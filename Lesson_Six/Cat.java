1598a4b7be65429f651918e64a3956367bfd9920
VDF <frolovi97@mail.ru>
2022-04-28 21:58:00 +0300

    ДЗ к уроку 6
    По возможности вернуться и переделать

---
+++ Lesson_Six/Cat.java
@@ -0,0 +1,8 @@
+package Lesson_Six;
+
+public class Cat extends Animal{
+
+    public Cat(int maximumRunningDistance, int maximumJumpingDistace){
+        super(maximumRunningDistance, maximumJumpingDistace,0);
+    }
+}
